06c8b7e4d90712a3a3c12ec35913c4e3b32a9ef6
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-25 19:27:43 +0800

    Initial Commit

---
+++ Peep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peep
+//
+//  Created by Adon Omeri on 25/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peep/PeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeepApp.swift
+//  Peep
+//
+//  Created by Adon Omeri on 25/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct PeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
